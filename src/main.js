bcffbb6caad0f945a595cb177485bd772f7b1cb2
jeremie <jeremie.herzog@edu.univ-fcomte.fr>
2020-11-28 10:33:51 +0100

    installation Youtube API

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
+
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+
 
 Vue.config.productionTip = false;
 
