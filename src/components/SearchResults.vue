4461d4f089758d9efa99a5f2afec4928637ba2f7
jeremie <jeremie.herzog@edu.univ-fcomte.fr>
2020-12-05 09:31:57 +0100

    inital Vue router

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -2,7 +2,6 @@
   <div class="container mb-3">
     <div class="d-flex mb-3">
       <div class="mr-auto">
-        <h3>Search Results for "{{ reformattedSearchString }}"</h3>
       </div>
       <div class="btn-group ml-auto" role="group">
         <button
---
+++ src/views/Home.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+    <TheNavigation />
+    <Accueil />
+    <SearchForm v-on:search="search"/>
+    <SearchResults
+      v-if="videos.length > 0"
+      v-bind:videos="videos"
+      v-bind:reformattedSearchString="reformattedSearchString"
+    />
+    <Pagination
+      v-if="videos.length > 0"
+      v-bind:prevPageToken="api.prevPageToken"
+      v-bind:nextPageToken="api.nextPageToken"
+      v-on:prev-page="prevPage"
+      v-on:next-page="nextPage"
+    />
+  </div>
+</template>
+<script>
+import Accueil from "../components/Accueil";
+import TheNavigation from "../components/layout/TheNavigation";
+import SearchForm from '../components/SearchForm';
+import SearchResults from '../components/SearchResults';
+import Pagination from '../components/Pagination';
+
+export default {
+  name: "App",
+  components: {
+    Accueil,
+    TheNavigation,
+    SearchForm,
+    SearchResults,
+    Pagination,
+  },
+    data() {
+    return {
+      videos: [],
+      reformattedSearchString: '',
+      api: {
+        baseUrl: 'https://www.googleapis.com/youtube/v3/search?',
+        part: 'snippet',
+        type: 'video',
+        order: 'viewCount',
+        maxResults: 12,
+        q: '',
+        key: 'AIzaSyDjWIVyR4ksdPEOJIw1sdV-WJWKUBTR-Ko' ,
+        prevPageToken: '',
+        nextPageToken: ''
+         }
+       }
+
+    },
+methods: {
+    search(searchParams) {
+      this.reformattedSearchString = searchParams.join(' ');
+      this.api.q = searchParams.join('+');
+      const { baseUrl, part, type, order, maxResults, q, key } = this.api;
+      const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&q=${q}&maxResults=${maxResults}&key=${key}`;
+      this.getData(apiUrl);
+    },
+
+    prevPage() {
+      const { baseUrl, part, type, order, maxResults, q, key, prevPageToken } = this.api;
+      const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&q=${q}&maxResults=${maxResults}&key=${key}&pageToken=${prevPageToken}`;
+      this.getData(apiUrl);
+    },
+
+    nextPage() {
+      const { baseUrl, part, type, order, maxResults, q, key, nextPageToken } = this.api;
+      const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&q=${q}&maxResults=${maxResults}&key=${key}&pageToken=${nextPageToken}`;
+      this.getData(apiUrl);
+    },
+    getData(apiUrl) {
+      this.$axios
+        .get(apiUrl)
+        .then(res => {
+          this.videos = res.data.items;
+          this.api.prevPageToken = res.data.prevPageToken;
+          this.api.nextPageToken = res.data.nextPageToken;
+        })
+        .catch(error => console.log(error));
+    }
+  }
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+
+#app {
+  background-color: white;
+
+}
+</style>
